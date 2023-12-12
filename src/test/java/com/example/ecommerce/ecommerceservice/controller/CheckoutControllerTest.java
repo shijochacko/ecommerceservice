7815e145c3b4df6eaa99d40c7ba5e8a44cff9377
SHIJO CHACKO <shijomeempillil@gmail.com>
2023-12-12 18:40:42 +0100

    Added integration test case with success case

--- src/test/java/com/example/ecommerce/ecommerceservice/controller/CheckoutControllerTest.java
+++ src/test/java/com/example/ecommerce/ecommerceservice/controller/CheckoutControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.ecommerceservice.controller;
 
+import com.example.ecommerce.ecommerceservice.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +12,32 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CheckoutControllerTest {
 
-    private final MockMvc mockMvc;
-
     @Autowired
-    public CheckoutControllerTest(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;
-    }
+    private MockMvc mockMvc;
+    @Autowired
+    private  ProductRepository productRepository;
 
+    @Test
+    void performCheckout_shouldGetSuccessResponse() throws Exception {
+
+        String input = new ObjectMapper().writeValueAsString(List.of("0005","0005","0005"));
+        mockMvc.perform(post("/api/v1/checkout")
+                        .contentType(MediaType.APPLICATION_JSON).content(input))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.price").value(2000));
+
+    }
     @Test
     void performCheckout_shouldVeifyUnAuthorizedRequestError() throws Exception {
 
-        String input = new ObjectMapper().writeValueAsString(List.of("0001", "0002", "0001"));
+        String input = new ObjectMapper().writeValueAsString(List.of("0005", "0006", "0007"));
         mockMvc.perform(post("/api/v1/getTotal")
                         .contentType(MediaType.APPLICATION_JSON).content(input))
                 .andExpect(status().isForbidden());
