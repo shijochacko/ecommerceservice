94ad3641fe8ac51733b1a5f3d5dfb8c04a2f4d1a
SHIJO CHACKO <shijomeempillil@gmail.com>
2023-12-12 14:56:43 +0100

    Project structure from spring initializer with basic dependencies

---
+++ src/main/java/com/example/ecommerce/ecommerceservice/EcommerceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.ecommerceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce/ecommerceservice/EcommerceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.ecommerceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
